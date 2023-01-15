259a3b9340d9a137197ab7a5f814f549d9212451
Vodes <48185123+Vodes@users.noreply.github.com>
2023-01-15 01:30:17 +0100

    Extensions: remove IDatabaseObject

--- src/main/kotlin/moe/styx/Extensions.kt
+++ src/main/kotlin/moe/styx/Extensions.kt
@@ -2,8 +2,3 @@ package moe.styx
 
 fun Boolean.toInt() = if (this) 1 else 0
 fun Int.toBoolean() = this > 0
-
-interface IDatabaseObject {
-    abstract fun save(newID: String? = null): Boolean
-    abstract fun delete(): Boolean
-}
